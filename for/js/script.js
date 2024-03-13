33b1323b6d1e83b0a99910c0312a56e3673a8ab1
CiroMarra <tensha93@gmail.com>
2024-03-13 14:49:45 +0100

    init project

---
+++ for/js/script.js
@@ -0,0 +1,8 @@
+// Consegna:
+// Data una lista della spesa:
+//Stampare sulla pagina (anche brutalmente, basta che si vedano) gli elementi della lista individualmente.
+// Svolgete questo esercizio in 2 versioni: col ciclo for e col ciclo while.
+
+const list = ['Latte','Cacao','Insalata','Carote','Pomodori','Piatti pronti'];
+
+console.log(list);
