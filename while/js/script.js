606327dc754680747dc2b926ea79ba3ac015e698
CiroMarra <tensha93@gmail.com>
2024-03-13 15:25:48 +0100

    feat: start a while script

---
+++ while/js/script.js
@@ -0,0 +1,10 @@
+// Consegna:
+// Data una lista della spesa:
+//Stampare sulla pagina (anche brutalmente, basta che si vedano) gli elementi della lista individualmente.
+// Svolgete questo esercizio in 2 versioni: col ciclo for e col ciclo while.
+
+
+// creo una variabile per selezionare l'elemento del DOM con id: "lista-spesa"
+const myLi = document.querySelector('#lista-spesa');
+// controllo quello che c'è all'interno dell'array
+ const list = ['Latte','Cacao','Insalata','Carote','Pomodori','Piatti pronti'];
